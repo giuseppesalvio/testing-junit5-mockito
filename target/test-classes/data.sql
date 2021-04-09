35a77fa4cfd6ef35417e32f8248adbe285eb5cd9
emanuele.castagnaro <emanuele.castagnaro@finconsgroup.com>
2021-04-09 09:41:53 +0200

    push last version del 9/4/2021

---
+++ target/test-classes/data.sql
@@ -0,0 +1,33 @@
+
+CREATE TABLE ANAGRAFICA (
+                            id INT  PRIMARY KEY,
+                            nome NCHAR(20),
+                            cognome  NCHAR(20),
+                            codiceFiscale  NCHAR(20)
+);
+
+
+CREATE TABLE Polizza (
+
+                          id INT  PRIMARY KEY,
+                          idContraente INT,
+                          idAssicurato  INT,
+                          idBeneficiario INT
+);
+
+
+CREATE TABLE ANAGRAFICAProva (
+                                 idAnagrafica INT  PRIMARY KEY,
+                                 nome NCHAR(20),
+                                 cognome  NCHAR(20),
+                                 codiceFiscale  NCHAR(20)
+);
+
+
+CREATE TABLE PolizzaProva (
+
+                         numeroPolizza INT  PRIMARY KEY,
+                         idContraente INT,
+                         idAssicurato  INT,
+                         idBeneficiario INT
+);
