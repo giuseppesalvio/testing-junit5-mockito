e2b106f562fa7a0dda41aa28055c5ff722c5555d
g.salvio <giuseppe.salvio@finconsgroup.com>
2021-04-23 17:50:10 +0200

    init

--- src/test/java/com/salvio/esercizio/nuovo/controller/PersonaControllerTestE2E.java
+++ src/test/java/com/salvio/esercizio/nuovo/controller/PersonaControllerTestE2E.java
@@ -11,6 +11,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
@@ -24,27 +26,35 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(classes = StartApplication.class)
+@SpringBootTest
 @AutoConfigureMockMvc
 class PersonaControllerTestE2E {
 
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     Gson gson = new Gson();
 
     @Test
     void personaGetByNomeCognome() throws Exception {
+
+        jdbcTemplate.update(
+                "insert into PERSONA(id ,nome   ,cognome ,codiceFiscale    ,indirizzo) values (1, 'ciccio', 'pasticcio', 'ciccioCF', 2)");
+
         NomeCognomeDto asd = new NomeCognomeDto("ciccio", "pasticcio");
         ResultActions resultActions =
                 this.mockMvc.perform(
                         post("/persona/getByNomeCognome")
-                        .content(gson.toJson(asd))
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(gson.toJson(asd))
                 )
-                .andDo(print()).andExpect(status().isOk());
+                        .andDo(print()).andExpect(status().isOk());
 
 
         String respo = resultActions.andReturn().getResponse().getContentAsString();
-        PersonaDto actual=gson.fromJson(respo,PersonaDto.class);
+        PersonaDto actual = gson.fromJson(respo, PersonaDto.class);
 
 
         PersonaDto expected = new PersonaDto(1, 2);
