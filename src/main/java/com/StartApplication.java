ef56d32bed2a43f14dff1dae0951c97b3c9fec76
Giuseppe <Salvio>
2021-05-06 15:58:51 +0200

    init

--- src/main/java/com/StartApplication.java
+++ src/main/java/com/StartApplication.java
@@ -1,23 +1,15 @@
 package com;
 
-import com.arca.repository.BookRepository;
 import com.arca.repository.StatisticheAccessoRepository;
-import com.entity.Book;
 import com.entity.StatisticheAccesso;
-import com.service.ExcelBook;
 import com.service.ExcelStatisticheAccesso;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
@@ -34,17 +26,16 @@ public class StartApplication {
     @EventListener(ApplicationReadyEvent.class)
     public void doSomethingAfterStartup() {
 
+        statisticheAccesso();
 
+    }
+
+    private void statisticheAccesso() {
         ExcelStatisticheAccesso excelStatisticheAccesso = new ExcelStatisticheAccesso();
 
-//        List<Book> bookList = new ArrayList<>();
-//        bookList.add(new Book("Head First Java", "Kathy Serria", 79));
-//        bookList.add(new Book("Effective Java", "Joshua Bloch", 36));
-//        bookList.add(new Book("Clean Code", "Robert martin", 42));
-//        bookList.add(new Book("Thinking in Java", "Bruce Eckel", 35));
-
-
         List<StatisticheAccesso> list = statisticheAccessoRepository.retrive();
+
+
         String excelFilePath = "NiceJavaBooks.xls";
 
         try {
