4705428004e8b7c9750969368cc8193f36e89368
g.salvio <giuseppe.salvio@finconsgroup.com>
2021-05-07 17:53:21 +0200

    refactor

--- src/test/java/com/arca/component/WriterIntegrationTest.java
+++ src/test/java/com/arca/component/WriterIntegrationTest.java
@@ -1,21 +1,18 @@
 package com.arca.component;
 
-import static com.arca.component.Writer.NOME_FILE_EXCEL;
-import static java.util.Arrays.asList;
-import static java.util.Collections.emptyList;
-import static org.apache.commons.io.FileUtils.directoryContains;
-import static org.assertj.core.api.Assertions.assertThat;
-
 import com.StartApplication;
-import com.arca.entity.ConteggioAccessi;
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.io.File;
+import java.io.IOException;
+
+import static com.arca.component.Writer.NOME_FILE_EXCEL;
+import static org.apache.commons.io.FileUtils.directoryContains;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(classes = {StartApplication.class})
 class WriterIntegrationTest {
